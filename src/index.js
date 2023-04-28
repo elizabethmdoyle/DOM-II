9fc16e69f08d5d2eb8da7f507a8f667947d55c01
Elizabeth Doyle <myoddjourney@gmail.com>
2023-04-28 19:29:58 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './less/index.less'
 
 // Your code goes here!
+
+/*
+* [ ] `mouseover`
+* [ ] `keydown`
+* [ ] `wheel`
+* [ ] `load`
+* [ ] `focus`
+* [ ] `resize`
+* [ ] `scroll`
+* [ ] `select`
+* [ ] `dblclick`
+* [ ] `drag / drop`*? 
+
+first need to select the elements from the dom
+once selected, functions need to be created for each event
+each function will have a corresponding event listener, to be tied to the event happening inside of the function
+*/
+
+//mouseover logo event
+
+
+
+const logo = document.querySelector('.logo-heading');
